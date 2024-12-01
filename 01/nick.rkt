726efa5f54c642db9ec91c3d9d43622005dd7b0d
Nick Thompson <git@nickt.dev>
2024-12-01 12:26:36 -0600

    nick day 1 cleanup

--- 01/nick.rkt
+++ 01/nick.rkt
@@ -1,7 +1,9 @@
 #lang racket
 
 (define lines (file->lines "01/nick.txt"))
-(define parsed (map (λ (l) (map string->number (string-split l))) lines))
-(define-values (left right) (apply values (apply map list parsed)))
-(println (foldl (λ (l r acc) (+ acc (abs (- l r)))) 0 (sort left <) (sort right <)))
-(println (foldl (λ (v acc) (+ acc (* v (count (curry = v) right)))) 0 left))
+(define parsed (map (compose (curry map string->number) string-split) lines))
+(define-values (left right) (apply values (apply map (compose (curryr sort <) list) parsed)))
+(define part-1 (foldl (λ (l r acc) (+ acc (abs (- l r)))) 0 left right))
+(define part-2 (foldl (λ (v acc) (+ acc (* v (count (curry = v) right)))) 0 left))
+part-1
+part-2
